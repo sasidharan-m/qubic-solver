032cff62f98d014869d93128deb28c8769ff0c64
sasidharan-m <samahali@ucsc.edu>
2019-03-17 18:45:39 -0700

    adding updated README and main with extra user options

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# qubic-solver
-Deep RL solver for qubic - (3 Dimensional Tic Tac Toe)
+# alphazero-solver
+Implementation of the AlphaZero algorithm for qubic - (3 Dimensional Tic Tac Toe)
+and connect4.
+
+Authors:
+Sasidharan Mahalingam (Overall framework integration, modyfing and fixes bugs
+  in the alpha zero implementation)
+Rafael Espericueta (Implementation of the Simple Heuristic Agent)
+Eliana Stefani (Implementation of MiniMax Agent)
+
+Packages Required:
+Python - 3.5 or later
+Tensorflow - 1.12 or later
+(Tensorflow-gpu with a working GPU accelarator recommended)
+Atleast 400 GB of free space on disk recommeded to saved the models trained
+
+Instructions to Train an AlphaZero Agent for Connect4:
+
+Instructions to Train an AlphaZero Agent for Qubic:
+
+Instructions to run the trained model for connect4:
+
+Instructions to run the trained model for qubic:
+
+Acknowledgement:  
+The framework is based on the implementation of Surag Nair for the game Othello
--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from Coach import Coach
-from qubic.QubicGame import QubicGame as Game
-from qubic.tensorflow.NNet import NNetWrapper as nn
+from connect4.Connect4Game import Connect4Game as Game1
+from connect4.tensorflow.NNet import NNetWrapper as nn1
+from qubic.QubicGame import QubicGame as Game2
+from qubic.tensorflow.NNet import NNetWrapper as nn2
 from utils import *
+import sys, getopt
 
-args = dotdict({
-    'numIters': 10,
+args1 = dotdict({
+    'numIters': 1000,
     'numEps': 100,
     'tempThreshold': 45,
     'updateThreshold': 0.6,
     'maxlenOfQueue': 200000,
     'numMCTSSims': 50,
+    'arenaCompare': 40,
+    'cpuct': 1,
+
+    'checkpoint': '.connect4/temp/',
+    'load_model': False,
+    'load_folder_file': ('connect4/dev/models/8x100x50','connect4/best.pth.tar'),
+    'numItersForTrainExamplesHistory': 20,
+
+})
+
+args2 = dotdict({
+    'numIters': 1000,
+    'numEps': 100,
+    'tempThreshold': 180,
+    'updateThreshold': 0.6,
+    'maxlenOfQueue': 200000,
+    'numMCTSSims': 200,
     'arenaCompare': 40,
     'cpuct': 1,
 
@@ -20,15 +40,53 @@ args = dotdict({
 
 })
 
+def main(argv):
+    game_type = ''
+    try:
+        opts, args = getopt.getopt(argv,"hg:",["help","game="])
+    except getopt.GetoptError:
+        print('main.py --game <game name> (game name is either connect4 or qubic)')
+        sys.exit(2)
+    if len(opts) != 1:
+        print('main.py --game <game name> (game name is either connect4 or qubic)')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('main.py --game <game name> (game name is either connect4 or qubic)')
+            sys.exit()
+        elif opt in ('-g', '--game'):
+            game_type = arg
+
+    if(game_type == 'connect4'):
+        print('Training agent for game connect4...')
+        g = Game1(6)
+        nnet = nn1(g)
+
+        if args1.load_model:
+            nnet.load_checkpoint(args1.load_folder_file[0], args1.load_folder_file[1])
+
+        c = Coach(g, nnet, args1)
+        if args1.load_model:
+            print("Load trainExamples from file")
+            c.loadTrainExamples()
+        c.learn()
+    elif(game_type == 'qubic'):
+        print('Training agent for game qubic...')
+        g = Game2(4,4,4)
+        nnet = nn2(g)
+
+        if args2.load_model:
+            nnet.load_checkpoint(args2.load_folder_file[0], args2.load_folder_file[1])
+
+        c = Coach(g, nnet, args2)
+        if args2.load_model:
+            print("Load trainExamples from file")
+            c.loadTrainExamples()
+        c.learn()
+    else:
+        print('Invalid game. Valid games are:')
+        print('connect4')
+        print('qubic')
+
 if __name__=="__main__":
-    g = Game(4,4,4)
-    nnet = nn(g)
-
-    if args.load_model:
-        nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
-
-    c = Coach(g, nnet, args)
-    if args.load_model:
-        print("Load trainExamples from file")
-        c.loadTrainExamples()
-    c.learn()
+    main(sys.argv[1:])
